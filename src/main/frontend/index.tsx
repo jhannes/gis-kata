4261d4c5a9e2a078fb654b2eb4ae3fd10d3c8f20
Johannes Brodwall <johannes@brodwall.com>
2023-02-02 01:37:34 +0100

    Introduce react in frontend

---
+++ src/main/frontend/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<h1>Hello, React!</h1>);
--- src/main/java/com/soprasteria/gisdemo/GisServer.java
+++ src/main/java/com/soprasteria/gisdemo/GisServer.java
@@ -2,6 +2,7 @@ package com.soprasteria.gisdemo;
 
 import org.eclipse.jetty.server.CustomRequestLog;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -10,8 +11,17 @@ public class GisServer {
 
     private void start() throws Exception {
         server.setRequestLog(new CustomRequestLog());
-        server.setHandler(new WebAppContext(Resource.newClassPathResource("/web"), "/"));
+        server.setHandler(createWebApp());
         server.start();
+    }
+
+    private static WebAppContext createWebApp() {
+        var baseResource = Resource.newClassPathResource("/web");
+        var webAppContext = new WebAppContext(baseResource, "/");
+        if (baseResource.getURI().getScheme().equals("file")) {
+            webAppContext.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        }
+        return webAppContext;
     }
 
     public static void main(String[] args) throws Exception {
