1e94a11ef4a0567248ae665f6951ea2c46d1744e
Johannes Brodwall <johannes@brodwall.com>
2023-03-18 22:47:28 +0100

    Structure for useMapLayer

--- src/modules/areasSidebar/areasSidebar.tsx
+++ src/modules/areasSidebar/areasSidebar.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+import VectorLayer from "ol/layer/Vector";
+import { Layer } from "ol/layer";
+
+const MapContext = React.createContext({
+  setMapLayers: (setter: (old: Layer[]) => Layer[]) => {},
+});
+
+function useMapContext() {
+  return useContext(MapContext);
+}
+
+function useMapLayer(layer: Layer) {
+  const { setMapLayers } = useMapContext();
+  useEffect(() => {
+    setMapLayers((old) => [...old, layer]);
+    return () => setMapLayers((old) => old.filter((l) => l !== layer));
+  }, []);
+}
 
 export function AreasSidebar() {
+  useMapLayer(
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: "/gis-kata/geojson/areas.json",
+      }),
+    })
+  );
+
   return <h2>Areas</h2>;
 }
