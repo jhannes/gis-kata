db2bb53e0882ac3238bd28a08812c37ad3008ae9
Johannes Brodwall <johannes@brodwall.com>
2023-03-18 22:26:18 +0100

    Introduce openlayers

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useMemo, useRef } from "react";
+import { Map } from "ol";
 
 export function MapView() {
-  return <div id="map">I'm a map</div>;
+  const map = useMemo(() => new Map(), []);
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, [map, mapRef]);
+
+  return <div id="map" ref={mapRef} />;
 }
