5d746f84959aa75001c8fabdd27076c9a5fabcee
Johannes Brodwall <johannes@brodwall.com>
2023-03-18 22:33:59 +0100

    Introduce openlayers: display open street map (wrong coordinates)

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,8 +1,24 @@
 import React, { MutableRefObject, useEffect, useMemo, useRef } from "react";
-import { Map } from "ol";
+import { Map, View } from "ol";
+import { OSM } from "ol/source";
+import TileLayer from "ol/layer/Tile";
 
 export function MapView() {
-  const map = useMemo(() => new Map(), []);
+  const map = useMemo(
+    () =>
+      new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        view: new View({
+          center: [10.754, 59.9115],
+          zoom: 5,
+        }),
+      }),
+    []
+  );
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
