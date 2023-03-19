6d7b52308e789805f3652b76be6e9fa19c8269e7
Johannes Brodwall <johannes@brodwall.com>
2023-03-19 12:35:35 +0100

    Bugfix: render loop in SchoolsSidebar

--- src/modules/schools/schoolsSidebar.tsx
+++ src/modules/schools/schoolsSidebar.tsx
@@ -1,19 +1,22 @@
 import { PageHeader } from "../pageHeader";
-import React from "react";
+import React, { useMemo } from "react";
 import { useMapLayer } from "../map/mapHooks";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 
 export function SchoolsSidebar() {
-  useMapLayer(
-    new VectorLayer({
-      source: new VectorSource({
-        url: "/gis-kata/geojson/schools.json",
-        format: new GeoJSON(),
+  const schoolsLayer = useMemo(
+    () =>
+      new VectorLayer({
+        source: new VectorSource({
+          url: "/gis-kata/geojson/schools.json",
+          format: new GeoJSON(),
+        }),
       }),
-    })
+    []
   );
+  useMapLayer(schoolsLayer);
 
   return (
     <>
