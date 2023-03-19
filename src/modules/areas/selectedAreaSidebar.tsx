768f48e3fb24db0aefad9d7e81c4bc48e302c5d4
Johannes Brodwall <johannes@brodwall.com>
2023-03-19 12:48:05 +0100

    Refactor: move useMapFit to mapHooks.tsx

--- src/modules/areas/selectedAreaSidebar.tsx
+++ src/modules/areas/selectedAreaSidebar.tsx
@@ -1,25 +1,7 @@
 import { AreaFeatureCollectionDto, AreaFeatureDto } from "./areas";
 import { Link, useParams } from "react-router-dom";
-import React, { useEffect } from "react";
-import { MultiPolygonDto } from "../geo";
-import { useMapContext } from "../map";
-import { FitOptions } from "ol/View";
-import { MultiPolygon, Point } from "ol/geom";
-
-function useMapFit(geometry: MultiPolygonDto, options: FitOptions) {
-  const { view } = useMapContext();
-  useEffect(() => {
-    const center = view.getCenter()!;
-    const zoom = view.getZoom();
-    console.log({ center, zoom, geometry });
-    view.fit(new MultiPolygon(geometry.coordinates), options);
-    return () =>
-      view.fit(new Point(center), {
-        maxZoom: zoom,
-        duration: options.duration,
-      });
-  }, [geometry, options]);
-}
+import React from "react";
+import { useMapFit } from "../map/";
 
 function SelectedAreaSidebarView({ area }: { area: AreaFeatureDto }) {
   useMapFit(area.geometry, {
--- src/modules/map/mapHooks.tsx
+++ src/modules/map/mapHooks.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { Layer } from "ol/layer";
 import { useMapContext } from "./mapContext";
+import { MultiPolygonDto } from "../geo";
+import { FitOptions } from "ol/View";
+import { MultiPolygon, Point } from "ol/geom";
 
 export function useMapLayer(layer: Layer) {
   const { setLayers } = useMapContext();
@@ -8,4 +11,18 @@ export function useMapLayer(layer: Layer) {
     if (layer) setLayers((old) => [...old, layer]);
     return () => setLayers((old) => old.filter((l) => l !== layer));
   }, [layer]);
+}
+
+export function useMapFit(geometry: MultiPolygonDto, options: FitOptions) {
+  const { view } = useMapContext();
+  useEffect(() => {
+    const center = view.getCenter()!;
+    const zoom = view.getZoom();
+    view.fit(new MultiPolygon(geometry.coordinates), options);
+    return () =>
+      view.fit(new Point(center), {
+        maxZoom: zoom,
+        duration: options.duration,
+      });
+  }, [geometry, options]);
 }
