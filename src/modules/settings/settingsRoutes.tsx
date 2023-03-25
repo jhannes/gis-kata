9bfa260775c75b158570b199737107c7564da358
Johannes Brodwall <johannes@brodwall.com>
2023-03-25 17:07:03 +0100

    Drawing custom layers, create Settings routes

--- src/modules/settings/settingsRoutes.tsx
+++ src/modules/settings/settingsRoutes.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Route, Routes, useParams } from "react-router-dom";
+
+function ShowAllSettings() {
+  return <h2>Settings</h2>;
+}
+
+function ShowCustomLayer() {
+  const { id } = useParams();
+  return <h2>Settings {id}</h2>;
+}
 
 export function SettingsRoutes() {
-  return <h2>Settings</h2>;
+  return (
+    <Routes>
+      <Route path={"/"} element={<ShowAllSettings />} />
+      <Route path={"/:id"} element={<ShowCustomLayer />} />
+      <Route path={"*"} element={<h2>Invalid path</h2>} />
+    </Routes>
+  );
 }
