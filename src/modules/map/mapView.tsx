e8ebcdcb8e57d67edf4eb36022eddcae03e4585b
Johannes Brodwall <johannes@brodwall.com>
2023-04-05 18:32:53 +0200

    Add openlayers to the map (showing null island)

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
 import { MutableRefObject, useEffect, useMemo, useRef } from "react";
-import { Map } from "ol";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
 
 export function MapView() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const map = useMemo(() => new Map(), []);
+  const map = useMemo(() => {
+    return new Map({
+      layers: [new TileLayer({ source: new OSM() })],
+      view: new View({
+        center: [10.7, 59.9],
+        zoom: 5,
+      }),
+    });
+  }, []);
 
   useEffect(() => map.setTarget(mapRef.current), [map, mapRef]);
 
