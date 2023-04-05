77a1b60267762a6e42b1724b2c7eb8d6aadbb1a5
Johannes Brodwall <johannes@brodwall.com>
2023-04-05 17:30:00 +0200

    Configure typescript
    * `npm install --save-dev @types/react @types/react-dom`
    *  `tsconfig.json`: `"jsx": "react""`

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
+import * as ReactDOM from "react-dom/client";
+import * as React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<h1>I'm a map</h1>);
