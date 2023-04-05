54c52c07a0e97e64cabdcbb91e93f10972d03dfa
Johannes Brodwall <johannes@brodwall.com>
2023-04-05 17:42:28 +0200

    Create `<Application />` structure

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
 export function Application() {
-  return <h1>I'm a map application</h1>;
+  return (
+    <>
+      <header>
+        <h1>I'm a map application</h1>
+      </header>
+      <main>
+        <nav>Navigation</nav>
+        <div id="map">I'm a map</div>
+        <aside>Details</aside>
+      </main>
+      <footer>Built with OpenLayers</footer>
+    </>
+  );
 }
