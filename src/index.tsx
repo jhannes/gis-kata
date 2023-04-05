e93d3960efe1317236607daa77b7357d83f74e4f
Johannes Brodwall <johannes@brodwall.com>
2023-04-05 17:35:34 +0200

    Extract `<Application />` to `modules/application`

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as ReactDOM from "react-dom/client";
 import * as React from "react";
+import { Application } from "./modules/application";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>I'm a map</h1>);
+
+root.render(<Application />);
---
+++ src/modules/application/application.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function Application() {
+  return <h1>I'm a map application</h1>;
+}
