14ccec7eb78cd137a94e23ddb3b43f9d36ffd85f
Johannes Brodwall <johannes@brodwall.com>
2023-04-05 17:48:31 +0200

    Add openlayers to the map (not showing anything)

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
+import { MutableRefObject, useEffect, useMemo, useRef } from "react";
+import { Map } from "ol";
 
 export function MapView() {
-  return <div id="map">I'm a map</div>;
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const map = useMemo(() => new Map(), []);
+
+  useEffect(() => map.setTarget(mapRef.current), [map, mapRef]);
+
+  return <div id="map" ref={mapRef} />;
 }
