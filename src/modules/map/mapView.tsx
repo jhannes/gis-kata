9f144b0ca0497db0558a6ac82c2cee158cc7503f
Johannes Brodwall <johannes@brodwall.com>
2023-04-07 23:57:33 +0200

    Show map (lots of issues)

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,14 +1,25 @@
 import * as React from "react";
 import { MutableRefObject, useEffect, useMemo, useRef } from "react";
 
-import { Map } from "ol";
+import { Map, View } from "ol";
+import { OSM } from "ol/source";
+import TileLayer from "ol/layer/Tile";
 
 export function MapView() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const map = useMemo(() => new Map(), []);
+  const map = useMemo(() => {
+    return new Map({
+      layers: [new TileLayer({ source: new OSM() })],
+      view: new View({
+        zoom: 5,
+        center: [10.7, 59.7],
+      }),
+    });
+  }, []);
 
   useEffect(() => {
     map.setTarget(mapRef.current);
+    return () => map.setTarget(undefined);
   }, [map, mapRef]);
 
   return <div id="map" ref={mapRef}></div>;
