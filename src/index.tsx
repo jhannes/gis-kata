eb5ed63dedbbd266440f047c66bae0983b397671
Johannes Brodwall <johannes@brodwall.com>
2023-04-07 23:31:44 +0200

    Extract <Application /> to src/modules/application/

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
+import { Application } from "./modules/application";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello React</h1>);
+
+root.render(<Application />);
