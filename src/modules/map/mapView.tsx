771aa868ef716ee9b536256d7b1d9da0cad574a3
Johannes Brodwall <johannes@brodwall.com>
2023-04-08 00:27:47 +0200

    Stubbed handleClickOnMap

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { MutableRefObject, useEffect, useMemo, useRef } from "react";
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 
 import { Feature, Map, MapBrowserEvent, View } from "ol";
 import { OSM } from "ol/source";
@@ -15,14 +15,23 @@ const backgroundLayer = new TileLayer({ source: new OSM() });
 export function MapView({
   onFeatureUnderPointer,
 }: {
-  onFeatureUnderPointer: (value: Feature<MultiPoint>) => void;
+  onFeatureUnderPointer: (value?: Feature<MultiPoint>) => void;
 }) {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const [clickedFeatures, setClickedFeatures] = useState<Feature<MultiPoint>[]>(
+    []
+  );
 
   function handlePointerMoveOnMap(event: MapBrowserEvent<MouseEvent>) {
     const features = map.getFeaturesAtPixel(event.pixel);
     onFeatureUnderPointer(
       features.length > 0 ? (features[0] as Feature<MultiPoint>) : undefined
+    );
+  }
+
+  function handleClickOnMap(event: MapBrowserEvent<MouseEvent>) {
+    setClickedFeatures(
+      map.getFeaturesAtPixel(event.pixel) as Feature<MultiPoint>[]
     );
   }
 
@@ -43,7 +52,11 @@ export function MapView({
 
   useEffect(() => {
     map.on("pointermove", handlePointerMoveOnMap);
-    return () => map.un("pointermove", handlePointerMoveOnMap);
+    map.on("click", handleClickOnMap);
+    return () => {
+      map.un("click", handleClickOnMap);
+      map.un("pointermove", handlePointerMoveOnMap);
+    };
   }, [map]);
 
   return <div id="map" ref={mapRef}></div>;
