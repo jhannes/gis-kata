7da4719885fd5f23d3cc9905da2dbdaa6d7ef463
Johannes Brodwall <johannes@brodwall.com>
2023-04-07 23:33:45 +0200

    Create dom structure

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export function Application() {
-  return <h1>Hello React Application</h1>;
+  return (
+    <>
+      <header>
+        <h1>Hello React Application</h1>
+      </header>
+      <main>
+        <nav>
+          <ul>
+            <li>Navigation</li>
+            <li>Settings</li>
+          </ul>
+        </nav>
+        <div id="map" />
+        <aside>Details</aside>
+      </main>
+      <footer>Geographic Web App demo</footer>
+    </>
+  );
 }
