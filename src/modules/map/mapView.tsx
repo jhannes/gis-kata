cd41b40525a7077019f94e625b78630b112c3374
Johannes Brodwall <johannes@brodwall.com>
2023-04-08 00:20:57 +0200

    Output feature under pointer

--- src/modules/map/mapView.tsx
+++ src/modules/map/mapView.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { MutableRefObject, useEffect, useMemo, useRef } from "react";
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 
-import { Map, View } from "ol";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
 import { OSM } from "ol/source";
 import TileLayer from "ol/layer/Tile";
 import { useGeographic } from "ol/proj";
 import { vgsLayer } from "./vgsLayer";
+import { MultiPoint } from "ol/geom";
 
 useGeographic();
 
@@ -13,6 +14,23 @@ const backgroundLayer = new TileLayer({ source: new OSM() });
 
 export function MapView() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const [featureUnderPointer, setFeatureUnderPointer] = useState<
+    Feature<MultiPoint> | undefined
+  >();
+
+  function handlePointerMoveOnMap(event: MapBrowserEvent<MouseEvent>) {
+    const features = map.getFeaturesAtPixel(event.pixel);
+    setFeatureUnderPointer(
+      features.length > 0 ? (features[0] as Feature<MultiPoint>) : undefined
+    );
+  }
+
+  useEffect(() => {
+    console.log({
+      featureUnderPointer: featureUnderPointer?.getProperties()?.skolenavn,
+    });
+  }, [featureUnderPointer]);
+
   const map = useMemo(() => {
     return new Map({
       layers: [backgroundLayer, vgsLayer],
@@ -27,6 +45,11 @@ export function MapView() {
     map.setTarget(mapRef.current);
     return () => map.setTarget(undefined);
   }, [map, mapRef]);
+
+  useEffect(() => {
+    map.on("pointermove", handlePointerMoveOnMap);
+    return () => map.un("pointermove", handlePointerMoveOnMap);
+  }, [map]);
 
   return <div id="map" ref={mapRef}></div>;
 }
