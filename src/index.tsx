60417b35e64a8640d4aec6902e7717342f67ba2d
Johannes Brodwall <johannes@brodwall.com>
2023-04-07 23:29:39 +0200

    `npm pkg set scripts.prettier="prettier --write src/"`

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello React</h1>)
+root.render(<h1>Hello React</h1>);
