ce7451a5037c1e1f03c30275391f7250474c4c49
Johannes Brodwall <johannes@brodwall.com>
2023-04-07 23:27:51 +0200

    Fix typescript compilation

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 
-const root = new ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<h1>Hello React</h1>)
